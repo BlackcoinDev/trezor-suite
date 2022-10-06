58e03099fc52317cc40d9bf6155c55e087dc14d3
mroz <mvarmuza@gmail.com>
2022-10-06 15:20:40 +0200

    chore(transport): unit tests in typescript

--- packages/transport/tests/build-receive.test.ts
+++ packages/transport/tests/build-receive.test.ts
@@ -1,9 +1,10 @@
-const { parseConfigure } = require('../src/lowlevel/protobuf/messages');
-const { buildOne } = require('../src/lowlevel/send');
-const { receiveOne } = require('../src/lowlevel/receive');
+import * as protobuf from 'protobufjs/light';
 
-const { buildBuffers } = require('../src/lowlevel/send');
-const { receiveAndParse } = require('../src/lowlevel/receive');
+import { buildOne } from '../src/lowlevel/send';
+import { receiveOne } from '../src/lowlevel/receive';
+
+import { buildBuffers } from '../src/lowlevel/send';
+import { receiveAndParse } from '../src/lowlevel/receive';
 
 const messages = {
     StellarPaymentOp: {
@@ -91,7 +92,7 @@ const fixtures = [
     },
 ];
 
-const parsedMessages = parseConfigure({
+const parsedMessages = protobuf.Root.fromJSON({
     nested: { hw: { nested: { trezor: { nested: { messages: { nested: messages } } } } } },
 });
 
@@ -102,7 +103,7 @@ describe('encoding json -> protobuf -> json', () => {
                 // encoded message
                 const encodedMessage = buildOne(parsedMessages, f.name, f.in);
                 // then decode message and check, whether decoded message matches original json
-                const decodedMessage = receiveOne(parsedMessages, encodedMessage);
+                const decodedMessage = receiveOne(parsedMessages, encodedMessage.toString('hex'));
                 expect(decodedMessage.type).toEqual(f.name);
                 expect(decodedMessage.message).toEqual(f.in);
             });
